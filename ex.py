d2c43dd7fe2b1def6472ae97316d5ffe1abdfd56
arthur1509 <131341323+Arthur-Fernandes-6@users.noreply.github.com>
2025-08-07 00:17:27 -0300

    Create ex.py

---
+++ ex.py
@@ -0,0 +1,66 @@
+#Q1
+#  nota1 = float(input("digite a nota1: "))
+# nota2 = float(input("digite a nota2: "))
+# nota3 = float(input("digite a nota3: "))
+
+# media = (nota1 + nota2 + nota3)/3
+
+# if media > 6:
+#     print("aprovado")
+
+# elif media == 6:
+#     print("FINAL")
+
+# else:
+#     print("reprovado")
+#////////////////////////////////////
+
+#Q2
+# numero = int (input ("digite um numero"))
+
+# if numero > 0:
+#     print ("é positivo")
+
+# elif numero < 0:
+#    print ("é negativo")
+
+# else:
+#     print("é zero") 
+#//////////////////////      
+
+# valor1 = int(input("digite o primeiro valor: "))
+# valor2 = int(input("digite o segundo valor: "))
+# operacao = input("digite a operaço que deseja realizar: ")
+
+# if(operacao == "+"):
+#    res = valor1 + valor2
+#    print(res)
+
+# elif(operacao == "-"):
+#    res = valor1 - valor2
+#    print(res)
+
+# elif(operacao == "/"):
+#    res = valor1/valor2
+#    print(res)
+#////////////////////////////
+
+#Q3
+# usuario = input("digite um usuario: ")
+# senha = input ("digite uma senha: ")
+
+# if (usuario == "admin" and senha == "admin"):
+#       print("bem vindo")
+#///////////////////////////////////////
+
+#Q4
+idade = int(input("digite sua idade: "))
+
+if idade >= 18:
+    print("voto obrigatorio")
+
+elif idade >= 16:
+    print("voto opcional")
+
+else:
+    print("não pode votar")
